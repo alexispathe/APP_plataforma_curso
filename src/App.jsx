e8becb0c31f229480ee78afde13217b436e63eaf
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-03 22:25:43 -0600

    Se instala el modulo de react-router-dom para trabajar con rutas, ademas se crea el componente de 'Home'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { componentPaths } from './routes/componentPaths';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 function App() {
 
 
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+          {componentPaths.map((component, i)=>(
+            <Route element={component.element} path={component.path} key={i}/>
+          ))}
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export const Home =()=>{
+    return(
+        <div className="container">
+            <h1>Home</h1>
+        </div>
+    )
+};
+
