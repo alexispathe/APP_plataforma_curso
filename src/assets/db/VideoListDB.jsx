3291b51e2952ad2a31e52f8dcad739849a29529e
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-04 16:58:18 -0600

    Se crean datos para simular videos de youtube y se manda a llamar en el componente de VideListComponent

---
+++ src/assets/db/VideoListDB.jsx
@@ -0,0 +1,10 @@
+export const VideoListDB= [
+    {
+        title: 'Las mejores peleas del anime', 
+        url: 'https://www.youtube.com/watch?v=UrDx3ZcPaf0'
+    },
+    {
+        title: 'Mi corazon encantado', 
+        url: 'https://www.youtube.com/watch?v=rPF_dL339F4'
+    },
+]
