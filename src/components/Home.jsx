a2a380e1c205c199a2d3b54d34d9c5d643d71bdc
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-04 17:32:45 -0600

    Se mejora la estructura para mandar videos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
 import { VideoListComponent } from "./VideoListComponent";
-import { useState } from "react";
 export const Home =()=>{
     // En estas variables se guardan los ID de todos los videos del canal de youtube, se mandaran como "props" dentro de "VideoListComponenet"
-    const [videoYTID, setVideoYTID] = useState([]); 
     return(
         <div className="container">
             <h1>Home</h1>
-            <VideoListComponent videoYTID={videoYTID} setVideoYTID={setVideoYTID}/>
+            <VideoListComponent/>
         </div>
     )
 };
--- src/components/VideoListComponent.jsx
+++ src/components/VideoListComponent.jsx
@@ -1,42 +1,48 @@
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { VideoComponent } from "./VideoComponent"; //Component que nos servira como render para cada video devuelto
-import {Spinner} from 'react-bootstrap';
+import { Spinner } from "react-bootstrap";
 import { VideoListDB } from "../assets/db/VideoListDB"; //Base de datos de los videos de youtube
-export const VideoListComponent = ({ videoYTID, setVideoYTID }) => {
-  //Con useEffect estamos mandando a llamar a la funcion getYoutubeVideos cuando el componente esta siendo llamado
+export const VideoListComponent = () => {
+    const [videoYT, setVideoYT] = useState([]); 
+  
+    //Con useEffect estamos mandando a llamar a la funcion getYoutubeVideos cuando el componente esta siendo llamado
   useEffect(() => {
     // getYoutubeVideosMethod2();
     getYoutubeVideos();
   }, []);
-  //Funcion que nos permitira devolver videos de un canal de youtube
-//   const getYoutubeVideosMethod2 = () => {
-//     const canal = "UCGmdxSvMG8cGJXwifpiJFtA";
-//     const key = "AIzaSyB4YAWtDV6VhlwY1wR-cthG43kTQdmtt10";
-//     const url = `https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${canal}&key=${key}`;
-//     fetch(url, {
-//       method: "GET",
-//       Accept: "application/json",
-//       mode: "cors",
-//     })
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data)
-//         setVideoYTID(data.items);
-//       })
-//       .catch((err) => console.log(err));
-//   };
-  const getYoutubeVideos =()=>{
-        setVideoYTID(VideoListDB);
-  }
+  
+  //   const getYoutubeVideosMethod2 = () => {
+  //     const canal = "UCGmdxSvMG8cGJXwifpiJFtA";
+  //     const key = "AIzaSyB4YAWtDV6VhlwY1wR-cthG43kTQdmtt10";
+  //     const url = `https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId=${canal}&key=${key}`;
+  //     fetch(url, {
+  //       method: "GET",
+  //       Accept: "application/json",
+  //       mode: "cors",
+  //     })
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log(data)
+  //         setVideoYTID(data.items);
+  //       })
+  //       .catch((err) => console.log(err));
+  //   };
+  const getYoutubeVideos = () => {
+    /* LLamamos a la base de datos para guardar la informacion de cada elemento*/ 
+    setVideoYT(VideoListDB);
+  };
   return (
     <div>
       <h1>Lista de videos</h1>
-      {videoYTID && videoYTID.length >= 1
-        ? videoYTID.map((video, i) => (
-            <VideoComponent key={i} id={video.url} title={video.title}/>
-          ))
-        : <div className="w-100 text-center"><Spinner variant="primary"/></div> //El Spinner  se ejecuta cuando  los videos aun no se devuelven
-    }
+      {
+        videoYT && videoYT.length >= 1 ? (
+          videoYT.map((video, i) => <VideoComponent key={i} data={video} />)
+        ) : (
+          <div className="w-100 text-center">
+            <Spinner variant="primary" />
+          </div>
+        ) //El Spinner  se ejecuta cuando  los videos aun no se devuelven
+      }
     </div>
   );
 };
