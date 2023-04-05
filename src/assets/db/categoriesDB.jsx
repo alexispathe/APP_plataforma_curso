7adac6a9950e298851883bdecdd20e9583afbcfd
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-05 13:01:20 -0600

    Se soluciona un pequeño bug donde se repetian items

--- src/assets/db/categoriesDB.jsx
+++ src/assets/db/categoriesDB.jsx
@@ -1,22 +1,22 @@
 export const categoriesDB =[
-    // {
-    //     name: 'Desarrollo WEB',
-    //     categoryURL: 'desarrollo-web',
-    //     categoryID: 'desarrollo-web-123',
-    //     sectionID: 'programacion-12'
-    // },
-    // {
-    //     name: 'Base de datos',
-    //     categoryURL: 'base-de-datos',
-    //     categoryID: 'base-de-datos-123',
-    //     sectionID: 'programacion-12',
-    // },
-    // {
-    //     name: 'Conferencia del cobaql',
-    //     categoryURL: 'conferencia-del-cobaq',
-    //     categoryID: 'conferencia-del-cobaq-123',
-    //     sectionID: 'conferencias-124',
-    // },
+    {
+        name: 'Desarrollo WEB',
+        categoryURL: 'desarrollo-web',
+        categoryID: 'desarrollo-web-123',
+        sectionID: 'programacion-12'
+    },
+    {
+        name: 'Base de datos',
+        categoryURL: 'base-de-datos',
+        categoryID: 'base-de-datos-123',
+        sectionID: 'programacion-12',
+    },
+    {
+        name: 'Conferencia del cobaql',
+        categoryURL: 'conferencia-del-cobaq',
+        categoryID: 'conferencia-del-cobaq-123',
+        sectionID: 'conferencias-124',
+    },
     {
         name: 'Teoria de la computacion',
         categoryURL: 'teoria-computacion',
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import { sectionsDB } from "../assets/db/sectionsDB";
 import { categoriesDB } from "../assets/db/categoriesDB";
 import { Link } from "react-router-dom";
 import "../assets/styles/header.css";
-import { Spinner } from "react-bootstrap";
-import { IoIosArrowDown } from "react-icons/io";
+import {Spinner} from 'react-bootstrap';
+import {IoIosArrowDown} from 'react-icons/io'
 export const Header = () => {
   const [sections, setSections] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -27,55 +27,26 @@ export const Header = () => {
               {sections && sections.length >= 1 ? (
                 <div className="header-section-container">
                   <ul className="header-section">
-                    {
-                      //Aqui estamos devolviendo todas las secciones de nuestra base de datos
-                      sections.map((section, i) => (
-                        <li
-                          className="header-section-item"
-                          key={section.sectionID || i}
-                        >
-                          <Link
-                            to={"/seccion/" + section.sectionURL}
-                            className="header-section-item-a"
-                          >
-                            {section.name} <IoIosArrowDown />
-                          </Link>
-                          <ul className="h-s-sub-item-container">
-                            {categories && categories.length >= 1 ? ( //Aqui estamos devolviendo todas categorias de BD para luego compararlos mediante su 'sectionID' y hacer una relacion
-                              categories.map((category, j) =>
-                                category.sectionID === section.sectionID ? (
-                                  <li
-                                    className="h-s-sub-item"
-                                    key={category.categoryID || j}
-                                  >
-                                    <Link to={"/seccion/" +section.sectionURL +"/" +category.categoryURL}>
-                                      {category.name}
-                                    </Link>
-                                  </li>
-                                ) : (
-                                  <p className="text-white">
-                                    ¡Estamos trabajando para traer contenido!
-                                  </p>
-                                )
-                              )
-                            ) : (
-                              <div className="text-center">
-                                <Spinner
-                                  animation="border"
-                                  className="text-white"
-                                />
-                              </div>
-                            )}
-                          </ul>
-                        </li>
-                      ))
-                    }
+                    {//Aqui estamos devolviendo todas las secciones de nuestra base de datos
+                    sections.map((section, i) => (
+                      <li className="header-section-item" key={section.sectionID || i}>
+                        <Link to={"/seccion/"+section.sectionURL} className="header-section-item-a">{section.name} <IoIosArrowDown/></Link>
+                        <ul className="h-s-sub-item-container" >
+                            {categories.length >=1? //Aqui estamos devolviendo todas categorias de BD para luego compararlos mediante su 'sectionID' y hacer una relacion 
+                                categories.map((category, j)=>(
+                                    category.sectionID ===  section.sectionID ? 
+                                    <li className="h-s-sub-item" key={category.categoryID || j}><Link to={"/seccion/"+ section.sectionURL+ "/"+category.categoryURL}>{category.name}</Link></li>
+                                    :''
+                                )):<div className="text-center"><Spinner animation="border" className="text-white" /></div>
+
+                            }                               
+                        </ul>
+                      </li>
+                    ))}
                   </ul>
                 </div>
               ) : (
-                <div className="text-center pt-5">
-                  <Spinner animation="border" className="text-white" />
-                </div>
+                <div className="text-center pt-5"><Spinner animation="border" className="text-white" /></div>
               )}
             </div>
           </div>
