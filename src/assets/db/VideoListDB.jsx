cb600e72b6cd97480156d917824741930f1a846f
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-05 20:36:01 -0600

    Se completa la fusion la rama header

---
+++ src/assets/db/VideoListDB.jsx
@@ -0,0 +1,36 @@
+export const VideoListDB= [
+    {
+        title: 'Las mejores peleas del anime', 
+        url: 'https://www.youtube.com/watch?v=UrDx3ZcPaf0',
+        videoInformation:{
+            categoryID: '',
+            subCategory: '',
+            creationDate: '',
+            authorID: '',
+        }
+
+    },
+    {
+        title: 'Changa', 
+        url: 'f0',
+        videoInformation:{
+            categoryID: '',
+            subCategory: '',
+            creationDate: '',
+            authorID: '',
+        }
+
+    },
+    {
+        title: 'Mi corazon encantado', 
+        url: 'https://www.youtube.com/watch?v=rPF_dL339F4',
+        videoInformation:{
+            categoryID: '',
+            subCategory: '',
+            creationDate: '',
+            authorID: '',
+        }
+       
+
+    },
+]
