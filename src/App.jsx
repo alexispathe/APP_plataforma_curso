c56e62c77dc10c24ce44d749ddafa07b201986d1
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-05 23:19:00 -0600

    Se adapatan los tamaños responsive para home y header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { componentPaths } from './routes/componentPaths';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
 import { Header } from './components/Header';
 function App() {
 
