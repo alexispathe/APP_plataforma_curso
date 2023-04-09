5c580c704cd1730a1cbaa44655afdd4672d0f95d
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-08 18:24:38 -0600

    Se agrega la funcionalidad de que se alamacen los videos vistos por el usuario en el loclastoarage

--- src/components/videoComponents/VideoComponent.jsx
+++ src/components/videoComponents/VideoComponent.jsx
@@ -4,12 +4,12 @@
 import "../../assets/styles/VideoComponent.css";
 import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, json } from "react-router-dom";
 export const VideoComponent = ({ data, width  }) => {
   const [idVideo, setIdVideo] = useState("");
   useEffect(() => {
     formatVideoURL();
-    // console.log(data)
+    saveWathedVideos();
   }, [data]);
   const formatVideoURL = () => {
     /*Dentro de esta funcion formatearemos la url del video de youtube para sacar solo el ID*/
@@ -17,6 +17,20 @@ export const VideoComponent = ({ data, width  }) => {
       setIdVideo(data.url.split("?v=")[1]);
     }
   };
+  const saveWathedVideos =()=>{
+    /*Esta funcion servira para guardar un historial de los videos que el usuario ha visto*/
+    if(localStorage.getItem('watched-videos')  && data.videoURL){
+      const watchedVideos = JSON.parse(localStorage.getItem('watched-videos'));
+      // buscamos que el video no este agregado previamente en el localstorage, si es asi devolvemos una lista de todos los videos menos ese para agregarlo al ultimo de la lista
+      const newVideo = watchedVideos.filter(video => video.videoURL !== data.videoURL)
+      newVideo.push(data)
+      localStorage.setItem('watched-videos', JSON.stringify(newVideo));
+    }else if(!localStorage.getItem('watched-videos') && data.videoURL ){
+      const videoPlayed = []
+      videoPlayed.push(data);
+      localStorage.setItem('watched-videos', JSON.stringify(videoPlayed));
+    }
+  }
   return (
     <>
       {idVideo && idVideo.length >= 1 ? (
