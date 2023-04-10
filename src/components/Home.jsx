4b66ff1ef9011511618070556d34887f0fa95623
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-10 15:07:15 -0600

    Se implementa el componente del ultimo video visto por el usuario

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,25 @@
 import '../assets/styles/home.css';
 import { VideoContainerComponent } from "./videoComponents/VideoContainerComponent";
 import { Search } from './Search';
+import { WatchedVideoComponent } from './videoComponents/WatchedVideoComponent';
+import { useEffect, useState } from 'react';
 export const Home =()=>{
+    const [status, setStatus] = useState(false);
+    useEffect(()=>{
+        showComponent();
+    },[])
+    const showComponent =()=>{
+        /*Esta funcion sirve para mostrar el componente del video visto por el usuario, en caso que exista */ 
+        if(localStorage.getItem('watched-videos')){
+            setStatus(true);
+        }
+    }
     return(
         <div className="home-container">
             <Search/>
             {/* <VideoListComponent/> */}
+            {status? <WatchedVideoComponent/> :''}
+            
         </div>
     )
 };
