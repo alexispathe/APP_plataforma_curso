5125fc31aa8a93d5202fca328a24cc9d1e0c53b4
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-20 12:42:35 -0600

    Se implementa el context de categorias en todos los componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { componentPaths } from './routes/componentPaths';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { Header } from './components/Header';
-import { VideosProvider } from './contexts/VideosProvider'; //ESTAMOS IMPORANTO EL CONTEXTO PARA VIDEOS
+import "bootstrap/dist/css/bootstrap.min.css";
+import { componentPaths } from "./routes/componentPaths";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { VideosProvider } from "./contexts/VideosProvider"; //ESTAMOS IMPORANTO EL CONTEXTO PARA VIDEOS
+import { SectionsProvider } from "./contexts/SectionProvider";
 function App() {
-
-
   return (
+    <SectionsProvider>
       <VideosProvider>
         <div className="App">
-
-        <BrowserRouter>
-        <Header/>
-          <Routes>
-            {componentPaths.map((component, i)=>(
-              <Route element={component.element} path={component.path} key={i}/>
-            ))}
-          </Routes>
-        </BrowserRouter>
-    </div>
-
+          <BrowserRouter>
+            <Header />
+            <Routes>
+              {componentPaths.map((component, i) => (
+                <Route
+                  element={component.element}
+                  path={component.path}
+                  key={i}
+                />
+              ))}
+            </Routes>
+          </BrowserRouter>
+        </div>
       </VideosProvider>
-
-  )
+    </SectionsProvider>
+  );
 }
 
-export default App
+export default App;
--- src/contexts/SectionProvider.jsx
+++ src/contexts/SectionProvider.jsx
@@ -1,9 +1,9 @@
 import { createContext, useState } from "react";
-import {SectionsDB} from '../assets/db/sectionsDB';
+import {sectionsDB} from '../assets/db/sectionsDB';
 export const SectionsContext = createContext(null);
 
-export const SectionsProviders = ({children})=>{
-    const [sectionDB, setSectionDB] = useState(SectionsDB);
+export const SectionsProvider = ({children})=>{
+    const [sectionDB, setSectionDB] = useState(sectionsDB);
     return (
         <SectionsContext.Provider value={[sectionDB, setSectionDB]}>{children}</SectionsContext.Provider>
     )
