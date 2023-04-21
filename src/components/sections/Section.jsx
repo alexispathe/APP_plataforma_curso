96f64fdb7807b6cd3477220202e060a7136e8d56
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-20 23:49:58 -0600

    Se mejoran los estilos de la seccion

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -1,6 +1,8 @@
 import { useState, useContext, useEffect } from "react";
-import "../../assets/styles/section.css";
+import { Card, Row, Col } from "react-bootstrap"; //Llamamos a las tarjetas y grid de bootstrap
 import { CategoryContext } from "../../contexts/CategoryProvider"; //Importamos el contexto donde estan las secciones
+import { Link } from "react-router-dom";
+import '../../assets/styles/Section.css';
 export const Section = ({ categoryType }) => {
   const [categoriesDB, setCategoriesDB] = useContext(CategoryContext);
   const [categories, setCategories] = useState([]);
@@ -18,22 +20,26 @@ export const Section = ({ categoryType }) => {
   };
   return (
     <div className="section-container">
-        <h1 className="text-center">Tecnologia </h1>
-      <div className="section">
+      <h1 className="text-center">Tecnologia </h1>
+      <Row className="">
         {categories && categories.length >= 1
           ? categories.map((category) => (
-              <div className="section-item">
-                <div className="category-image">
-                  <img  
-                    src={category.categoryInformation.image}
-                    alt={category.name}
-                  />
-                </div>
-                <div className="category-title">{category.name}</div>
-              </div>
+              <Col xs={12} sm={6} md={6} lg={4} xl={"auto"} className="card-container"   >
+                <Link to="" className="text-decoration-none">
+                  <Card style={{ width: "100%" }}>
+                    <Card.Img
+                      variant="top"
+                      src={category.categoryInformation.image}
+                    />
+                    <Card.Body>
+                      <Card.Title className="text-center">{category.name}</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </Link>
+              </Col>
             ))
           : ""}
-      </div>
+      </Row>
     </div>
   );
 };
