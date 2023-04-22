31704a9dd3ea4c188440ff2721978d35132473c4
Alexis Pathe <alexispatheguillen@gmail.com>
2023-04-22 13:46:51 -0600

    Esta en proceso el header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,77 +3,71 @@ import { sectionsDB } from "../assets/database/sectionsDB";
 import { categoriesDB } from "../assets/database/categoriesDB";
 import { Link } from "react-router-dom";
 import "../assets/styles/header.css";
-import {Spinner} from 'react-bootstrap';
+import {Spinner, Offcanvas} from 'react-bootstrap';
 import {IoIosArrowDown} from 'react-icons/io';
 export const Header = () => {
   const [sections, setSections] = useState([]);
   const [categories, setCategories] = useState([]);
+  // Estados para el offCanvas de bootstrap
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   useEffect(() => {
     setSections(sectionsDB);
     setCategories(categoriesDB);
   }, []);
  
-  const onBarsClick=()=>{
-    /*  
-        Cuando el usuario le de click al menu, este desaparecera y hara el menu oculto aparezca
-        
-    */ 
-    document.querySelector('.header-container').style.marginLeft = "0px";
-  }
-  const onCloseClick=()=>{
-    // Cuando el usuario le de click, se ocultara el menu
-    document.querySelector('.bars-container').style.display = "block";
-    document.querySelector('.header-container').style.marginLeft = "-100%";
-    
-  }
   return (
     <>
-     <div className="bars-container" onClick={onBarsClick}>
+     <div className="bars-container" onClick={handleShow}>
             &#9776;
         </div>
       <div className="header-container">
-     
-        <div className="header">
-        <div className="close-container" onClick={onCloseClick}>
-                &times;
+          <Offcanvas  show={show} onHide={handleClose}>
+            <div class="header">
+            <Offcanvas.Header  >
+                  <Offcanvas.Title>
+                    <div className="header-top">
+                      <div className="header-logo">
+                        <Link to="/">
+                          <img src="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon.png" />
+                        </Link>
+                      </div>
+                    </div>
+                  </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <div className="header-down">
+                      {sections && sections.length >= 1 ? (
+                        <div className="header-section-container">
+                          <ul className="header-section">
+                            {//Aqui estamos devolviendo todas las secciones de nuestra base de datos
+                            sections.map((section, i) => (
+                              <li className="header-section-item" key={section.sectionID || i}>
+                                <Link to={"/seccion/"+section.sectionURL} >{section.name} <IoIosArrowDown/></Link>
+                                <ul className="h-s-sub-item-container" >
+                                    {categories.length >=1? //Aqui estamos devolviendo todas categorias de BD para luego compararlos mediante su 'sectionID' y hacer una relacion 
+                                        categories.map((category, j)=>(
+                                            category.sectionID ===  section.sectionID ? 
+                                            <li className="h-s-sub-item" key={category.categoryID || j}><Link to={"/seccion/"+ section.sectionURL+ "/"+category.categoryInformation.categoryURL}><i className={category.iconName} style={{"margin-right": 5}} ></i>{category.name}</Link></li>
+                                            :''
+                                        )):<div className="text-center"><Spinner animation="border" className="text-white" /></div>
+                                    }                               
+                                </ul>
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      ) : (
+                        <div className="text-center pt-5"><Spinner animation="border" className="text-white" /></div>
+                      )}
+                    </div>
+              </Offcanvas.Body>
             </div>
-          <div className="header-top">
-           
-            <div className="header-logo">
-              <Link to="/">
-                <img src="https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon.png" />
-              </Link>
-            </div>
-
-            <div className="header-down">
-              {sections && sections.length >= 1 ? (
-                <div className="header-section-container">
-                  <ul className="header-section">
-                    {//Aqui estamos devolviendo todas las secciones de nuestra base de datos
-                    sections.map((section, i) => (
-                      <li className="header-section-item" key={section.sectionID || i}>
-                        <Link to={"/seccion/"+section.sectionURL} >{section.name} <IoIosArrowDown/></Link>
-                        <ul className="h-s-sub-item-container" >
-                            {categories.length >=1? //Aqui estamos devolviendo todas categorias de BD para luego compararlos mediante su 'sectionID' y hacer una relacion 
-                                categories.map((category, j)=>(
-                                    category.sectionID ===  section.sectionID ? 
-                                    <li className="h-s-sub-item" key={category.categoryID || j}><Link to={"/seccion/"+ section.sectionURL+ "/"+category.categoryInformation.categoryURL}><i className={category.iconName} style={{"margin-right": 5}} ></i>{category.name}</Link></li>
-                                    :''
-                                )):<div className="text-center"><Spinner animation="border" className="text-white" /></div>
-
-                            }                               
-                        </ul>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              ) : (
-                <div className="text-center pt-5"><Spinner animation="border" className="text-white" /></div>
-              )}
-            </div>
-          </div>
+             
+          </Offcanvas>
         </div>
-      </div>
     </>
   );
 };
