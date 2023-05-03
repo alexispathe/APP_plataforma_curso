af4552629d440f0861f5b800596af896a9966d48
Alexis Pathe <alexispatheguillen@gmail.com>
2023-05-03 17:34:43 -0600

    Se mejora el diseño del section

--- src/components/sections/Section.jsx
+++ src/components/sections/Section.jsx
@@ -1,4 +1,4 @@
-import '../../assets/styles/SectionComponent.css';
+import "../../assets/styles/SectionComponent.css";
 import { useState, useContext, useEffect } from "react";
 import { Card, Row, Col } from "react-bootstrap"; //Llamamos a las tarjetas y grid de bootstrap
 import { CategoryContext } from "../../contexts/CategoryProvider"; //Importamos el contexto donde estan las secciones
@@ -11,41 +11,39 @@ export const Section = ({ sectionType, name }) => {
   }, []);
   const filterCategories = (sectionType) => {
     /*Esta funcion permitira hacer un filtro de cada uno de los curso o seccion donde coencida la propiedad categoryTypecon*/
-    
+
     setCategories([
       ...categoriesDB.filter(
         (category) => category.sectionInformation.sectionType === sectionType
       ),
     ]);
-
   };
   return (
     <>
-      {categories && categories.length >= 1? 
-
-    <div className="section-container">
+      {categories && categories.length >= 1 ? (
+        <div className="section-container">
           <h1 className="text-center">{name}</h1>
-          <Row className="">
-             {categories.map((category) => (
-                  <Col xs={12} sm={6} md={6} lg={4} xl={"auto"} className="card-container"   >
-                    <Link to="" className="text-decoration-none">
-                      <Card style={{ width: "100%" }}>
-                        <Card.Img
-                          variant="top"
-                          src={category.categoryInformation.image}
-                        />
-                        <Card.Body>
-                          <Card.Title className="text-center">{category.name}</Card.Title>
-                        </Card.Body>
-                      </Card>
-                    </Link>
-                  </Col>
-                ))}
-          </Row>
-    </div>
-          : ""}
-
+          <div className="card-container d-flex overflow-auto ">
+            {categories.map((category) => (
+              <Card style={{ minWidth: "300px" }} className="card-item m-2">
+                <Link to="" className="text-decoration-none">
+                  <Card.Img
+                    variant="top"
+                    src={category.categoryInformation.image}
+                  />
+                  <Card.Body>
+                    <Card.Title className="text-center">
+                      {category.name}
+                    </Card.Title>
+                  </Card.Body>
+                </Link>
+              </Card>
+            ))}
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
     </>
-
   );
 };
